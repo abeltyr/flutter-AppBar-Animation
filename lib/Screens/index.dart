1884acf1630f9ada208901f24f31ad24a34fafea
avell21 <abeltyr@gmail.com>
2021-05-13 18:30:12 +0300

    (Feat) the scrollable done separetly

--- lib/Screens/index.dart
+++ lib/Screens/index.dart
@@ -6,14 +6,12 @@ class IndexScreen extends StatefulWidget {
 }
 
 class _IndexScreenState extends State<IndexScreen> {
-  double imageHeight = 240;
-  double idealImageHeight = 240;
-  double maxImageHeight = 280;
   double scrollStart = 0;
-  bool disappear = false;
   double firstLayerHeight = 400;
   double idealFirstLayerHeight = 400;
   double maxFirstLayerHeight = 450;
+  bool showtitle = false;
+  bool otherScroll = false;
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
@@ -23,63 +21,65 @@ class _IndexScreenState extends State<IndexScreen> {
         body: Stack(
           children: [
             Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 20,
-              ),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  stops: [
-                    firstLayerHeight > 120 ? 0.5 : 1,
-                    1,
-                  ],
-                  colors: [
-                    Color(0xFF674843),
-                    Color(0xFF191919),
-                  ],
-                ),
-              ),
               height: firstLayerHeight,
               width: width,
               child: SafeArea(
-                child: Column(
+                child: Stack(
                   children: [
                     Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 15,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: [
+                            firstLayerHeight > 150 ? 0.5 : 1,
+                            1,
+                          ],
+                          colors: [
+                            Color(0xFF674843),
+                            Color(0xFF191919),
+                          ],
+                        ),
                       ),
-                      child: !disappear
-                          ? Opacity(
-                              opacity: imageHeight / idealImageHeight < 1
-                                  ? imageHeight / idealImageHeight
-                                  : 1,
-                              child: Image.asset(
-                                "assets/images/view.png",
-                                cacheHeight: 720,
-                                cacheWidth: 720,
-                                height: imageHeight,
-                                fit: BoxFit.contain,
-                              ),
-                            )
-                          : Container(),
+                      padding: EdgeInsets.only(
+                          top: 20, bottom: firstLayerHeight > 150 ? 50 : 15),
+                      child: Stack(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(left: 20),
+                            alignment: Alignment.topLeft,
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                          ),
+                          Container(
+                              alignment: Alignment.topCenter,
+                              child: firstLayerHeight > 260
+                                  ? Opacity(
+                                      opacity: firstLayerHeight / 240 < 1
+                                          ? firstLayerHeight / 240
+                                          : 1,
+                                      child: Image.asset(
+                                        "assets/images/view.png",
+                                        cacheHeight: 720,
+                                        cacheWidth: 720,
+                                        fit: BoxFit.contain,
+                                      ),
+                                    )
+                                  : Text(
+                                      "Data",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    )),
+                        ],
+                      ),
                     ),
                   ],
-                ),
-              ),
-            ),
-            SafeArea(
-              child: Container(
-                margin: EdgeInsets.only(
-                    top:
-                        firstLayerHeight > 130 ? firstLayerHeight - 130 : 52.5),
-                height: height -
-                    (firstLayerHeight > 130 ? firstLayerHeight - 130 : 52.5),
-                width: width,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [Text("data")],
                 ),
               ),
             ),
@@ -94,52 +94,97 @@ class _IndexScreenState extends State<IndexScreen> {
                   // setup the upward motion action
                   if (scrollStart > data.globalPosition.dy) {
                     setState(() {
-                      disappear = imageHeight < 140;
-
-                      if (firstLayerHeight > 120)
+                      if (firstLayerHeight > 135)
                         firstLayerHeight = firstLayerHeight -
                             50 *
                                 ((scrollStart - data.globalPosition.dy) /
                                     height);
-                      if (imageHeight > 140)
-                        imageHeight = imageHeight -
-                            50 *
-                                ((scrollStart - data.globalPosition.dy) /
-                                    height);
+
+                      otherScroll = firstLayerHeight <= 136;
                     });
                   }
                   // setup the downward motion action
                   else if (scrollStart < data.globalPosition.dy) {
-                    print(firstLayerHeight);
-                    print(maxFirstLayerHeight);
                     setState(() {
                       if (firstLayerHeight < maxFirstLayerHeight)
                         firstLayerHeight = firstLayerHeight +
                             40 *
                                 ((data.globalPosition.dy - scrollStart) /
                                     height);
-
-                      if (firstLayerHeight > imageHeight + 100 &&
-                          imageHeight < 300) {
-                        disappear = !(imageHeight > 125);
-                        imageHeight = imageHeight +
-                            40 *
-                                ((data.globalPosition.dy - scrollStart) /
-                                    height);
-                      }
                     });
                   }
                 },
                 onVerticalDragEnd: (DragEndDetails data) {
                   setState(() {
-                    imageHeight = imageHeight > idealImageHeight
-                        ? idealImageHeight
-                        : imageHeight;
                     firstLayerHeight = firstLayerHeight > idealFirstLayerHeight
                         ? idealFirstLayerHeight
                         : firstLayerHeight;
                   });
                 },
+                child: SafeArea(
+                  child: Container(
+                    height: height,
+                    padding: EdgeInsets.only(
+                        top: firstLayerHeight > 55
+                            ? firstLayerHeight - 55
+                            : 52.5),
+                    width: width,
+                    child: NotificationListener<ScrollEndNotification>(
+                      onNotification: (scrollEnd) {
+                        var metrics = scrollEnd.metrics;
+                        if (metrics.atEdge) {
+                          if (metrics.pixels == 0) {
+                            // print('At top');
+                            setState(() {
+                              otherScroll = false;
+                            });
+                          }
+                          // print('At bottom');
+                        }
+                        return true;
+                      },
+                      child: ListView(
+                        physics: otherScroll
+                            ? AlwaysScrollableScrollPhysics()
+                            : NeverScrollableScrollPhysics(),
+                        children: [
+                          Text(
+                            "Data",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          Container(
+                            height: 400,
+                            width: width,
+                          ),
+                          Text(
+                            "Data",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          Container(
+                            height: 400,
+                            width: width,
+                          ),
+                          Text(
+                            "Data",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
